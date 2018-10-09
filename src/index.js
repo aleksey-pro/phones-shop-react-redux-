03bc7d675df718ba6e41d5f8ccc9df92ae43b7d0
dave_ghan <anxieter@yandex.ru>
2018-10-09 14:39:34 +0300

    Created routing and store layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { syncHistoryWithStore } from "react-router-redux";
+import { Router, Route, browserHistory } from "react-router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./main.css";
+import reducers from "reducers";
+import Layout from "containers/Layout";
+import Phones from "containers/Phones";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route component={Layout}>
+        <Route path="/" component={Phones} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
